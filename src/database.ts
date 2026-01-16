5f839c1dc8d6a32097998cfa369f4ff23a5f5b7e
dionromero <dionalcantara20@gmail.com>
2026-01-15 22:22:33 -0300

    feat: add database setup and required dependencies
    - Add postgres database connection setup with initialization function
    - Include Elysia framework and related dependencies for backend development
    - Add Zod for schema validation

---
+++ src/database.ts
@@ -0,0 +1,25 @@
+import postgres from "postgres"
+
+const sql = postgres({
+    host: "localhost", 
+    port: 5432,
+    database: "CRUD_USUARIOS",
+    username: "postgres",
+    password: "12345678"
+})
+
+export async function inicializarBanco(){
+    await sql`
+    CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY, 
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    idade INTENGER CHECK (idade >= 0 AND idade <= 150),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+    `;
+    console.log("banco de dados inicializado!!")
+}
+
+export default sql;
